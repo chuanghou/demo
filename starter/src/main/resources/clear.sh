7edcb35919e0ca3cd516eab66de451b37fd9bbc3
chuanghou <houchuang@outlook.com>
2023-07-29 15:01:58 +0800

    change deprecated class implementations

--- starter/src/main/resources/clear.sh
+++
@@ -1,5 +0,0 @@
-cd ~
-# shellcheck disable=SC2006
-pid = `ps -ef|grep cluster-app.jar |grep -v grep|awk '{print $2}'`
-kill -15 pid
-rm -rf *
--- starter/src/main/resources/start.sh
+++
@@ -1,2 +0,0 @@
-cd ~
-nohup java -jar starter-1.0.0-SNAPSHOT.jar > console.log 2>&1 &
