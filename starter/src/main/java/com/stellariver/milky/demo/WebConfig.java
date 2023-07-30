5afb0aaa160bfe66937eae279b1c72d1f5b8045f
houchuang <houchuang@outlook.com>
2023-07-30 14:54:50 +0800

    create ok

--- starter/src/main/java/com/stellariver/milky/demo/WebConfig.java
+++ starter/src/main/java/com/stellariver/milky/demo/WebConfig.java
@@ -29,10 +29,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                //是否发送Cookie
                 .allowCredentials(true)
-                //放行哪些原始域
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("*");
