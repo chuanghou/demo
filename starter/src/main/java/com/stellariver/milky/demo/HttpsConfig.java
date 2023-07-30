bfd72040665a9655e777e3779ea75bbc529f234a
houchuang <houchuang@outlook.com>
2023-07-30 14:59:46 +0800

    create ok

--- starter/src/main/java/com/stellariver/milky/demo/HttpsConfig.java
+++ starter/src/main/java/com/stellariver/milky/demo/HttpsConfig.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class HttpsConfig {
 
     @Value("${server.port}")
