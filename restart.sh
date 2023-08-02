91e518ffae183e1f61034a87414b96c073da1258
chuanghou <houchuang@outlook.com>
2023-08-02 09:12:06 +0800

    restart.sh

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@ ps -ef | grep java | grep MilkyDemoApplication | awk '{print $2}' | xargs kill -
 
 script_path=$(cd $(dirname $0);pwd)
 
-nohup java -cp "./bin/*:./lib/*" com.stellariver.milky.demo.MilkyDemoApplication -Dstatic.resource="${script_path}/static" >/dev/null 2>&1 &
+nohup java -cp "./bin/*:./lib/*" -Dstatic.resource="${script_path}/static/" com.stellariver.milky.demo.MilkyDemoApplication > console.log 2>&1 &
